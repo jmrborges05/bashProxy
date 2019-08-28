0651f507de400d5994975c2333c1b1ac8275b686
João Borges <sucrewar10@gmail.com>
2019-08-28 16:07:17 +0100

    Added proxy.sh file
    script to enable and disable http proxy on mac os and start mitmproxy servies

---
+++ proxy.sh
@@ -0,0 +1,38 @@
+#!/bin/bas -ih
+shopt -s expand_aliases
+PS3='Please enter your choice: '
+options=("Enable proxy" "Disable proxy" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Enable proxy")
+			#kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
+			lsof -ti tcp:8080 | xargs kill
+			networksetup -setwebproxystate Wi-Fi off
+			#get ip address of the machine 
+			ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+			echo "SET HTTP PROXY"
+			echo "SET PROXY FOR LOCAL IP  ${ip}"
+			#setup http proxy with the machine ip 
+			networksetup -setwebproxy Wi-Fi $ip 8080
+			networksetup -setwebproxystate Wi-Fi on
+			#open new terminal window and start mitmweb proxy portal
+			osascript -e 'tell application "Terminal" to do script "mitmweb"'
+			break
+            ;;
+        "Disable proxy")
+            #kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
+			lsof -ti tcp:8080 | xargs kill
+			networksetup -setwebproxystate Wi-Fi off
+			echo "HTTP PROXY DISABLED"
+			break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+
