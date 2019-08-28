29f9cfeee407e07cd36dbc9d4fa3fd73166e30d7
João Borges <sucrewar10@gmail.com>
2019-08-28 16:56:12 +0100

    updates on execution of mitmproxy

--- proxy.sh
+++ proxy.sh
@@ -12,16 +12,16 @@ do
 			#get ip address of the machine 
 			ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 			echo "SET HTTP PROXY"
-			echo "SET PROXY FOR LOCAL IP  ${ip}"
+			echo "SET PROXY FOR LOCAL IP :::: ${ip}"
 			#setup http proxy with the machine ip 
 			networksetup -setwebproxy Wi-Fi $ip 8080
 			networksetup -setwebproxystate Wi-Fi on
 			#open mitmweb proxy portal without any output on bash
-			mitmweb > /dev/null
+			nohup mitmweb &>/dev/null &
 			break
             ;;
         "Disable proxy")
-            #kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
+			#kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
 			lsof -ti tcp:8080 | xargs kill
 			networksetup -setwebproxystate Wi-Fi off
 			echo "HTTP PROXY DISABLED"
