06e5aa3006fe9122f11c9e6ac941e80826ec8de3
João Borges <sucrewar10@gmail.com>
2019-08-29 10:26:46 +0100

    updated stop proxy
    refactoring code
    Create functions to stop and enable proxy

--- proxy.sh
+++ proxy.sh
@@ -1,35 +1,45 @@
 #!/bin/bas -ih
 shopt -s expand_aliases
+
+function stopProxy() {
+    #kill all instances of proxies mitmweb listening and stop mac listening http proxy
+    ps -ef | grep mitmweb | grep -v grep | awk '{print $2}' | xargs kill -9
+    networksetup -setwebproxystate Wi-Fi off
+}
+
+function enableProxy() {
+    #get ip address of the machine
+    ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+    echo "SET HTTP PROXY"
+    echo "SET PROXY FOR LOCAL IP :::: ${ip}"
+    #setup http proxy with the machine ip
+    networksetup -setwebproxy Wi-Fi $ip 8080
+    networksetup -setwebproxystate Wi-Fi on
+    #open mitmweb proxy portal without any output on bash
+    nohup mitmweb &>/dev/null &
+	echo "PROXY SETUP AND ENABLE"
+}
+
+
+
 PS3='Please enter your choice: '
 options=("Enable proxy" "Disable proxy" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Enable proxy")
-			#kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
-			lsof -ti tcp:8080 | xargs kill
-			networksetup -setwebproxystate Wi-Fi off
-			#get ip address of the machine 
-			ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-			echo "SET HTTP PROXY"
-			echo "SET PROXY FOR LOCAL IP :::: ${ip}"
-			#setup http proxy with the machine ip 
-			networksetup -setwebproxy Wi-Fi $ip 8080
-			networksetup -setwebproxystate Wi-Fi on
-			#open mitmweb proxy portal without any output on bash
-			nohup mitmweb &>/dev/null &
-			break
-            ;;
+            stopProxy
+            enableProxy
+            break
+        ;;
         "Disable proxy")
-			#kill all instances of proxies listening on port 8080 and stop mac listening http proxy 
-			lsof -ti tcp:8080 | xargs kill
-			networksetup -setwebproxystate Wi-Fi off
-			echo "HTTP PROXY DISABLED"
-			break
-            ;;
+            stopProxy
+            echo "HTTP PROXY DISABLED"
+            break
+        ;;
         "Quit")
             break
-            ;;
+        ;;
         *) echo "invalid option $REPLY";;
     esac
 done
